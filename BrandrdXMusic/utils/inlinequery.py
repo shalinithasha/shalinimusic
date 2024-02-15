19d028c0e2844f0b445038d428d1404eb38f1c0b
GIgabyte <88191224+shalinithasha@users.noreply.github.com>
2024-02-15 19:57:02 +0530

    Update inlinequery.py

--- BrandrdXMusic/utils/inlinequery.py
+++ BrandrdXMusic/utils/inlinequery.py
@@ -7,19 +7,19 @@ answer.extend(
         InlineQueryResultArticle(
             title="Pᴀᴜsᴇ",
             description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://t3.ftcdn.net/jpg/05/63/40/42/360_F_563404257_7GReuNjZACa2FSw0anfRSI9zb8pUzIMt.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Rᴇsᴜᴍᴇ",
             description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://t3.ftcdn.net/jpg/05/63/40/42/360_F_563404257_7GReuNjZACa2FSw0anfRSI9zb8pUzIMt.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
             title="Sᴋɪᴩ",
             description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://t3.ftcdn.net/jpg/05/63/40/42/360_F_563404257_7GReuNjZACa2FSw0anfRSI9zb8pUzIMt.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
@@ -31,13 +31,13 @@ answer.extend(
         InlineQueryResultArticle(
             title="Sʜᴜғғʟᴇ",
             description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://t3.ftcdn.net/jpg/05/63/40/42/360_F_563404257_7GReuNjZACa2FSw0anfRSI9zb8pUzIMt.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
             title="Lᴏᴏᴩ",
             description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://t3.ftcdn.net/jpg/05/63/40/42/360_F_563404257_7GReuNjZACa2FSw0anfRSI9zb8pUzIMt.jpg",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
