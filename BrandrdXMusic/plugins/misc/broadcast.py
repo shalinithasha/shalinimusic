94c216166f53befe266ea1476f3d38fd1827c475
GIgabyte <88191224+shalinithasha@users.noreply.github.com>
2024-02-15 21:04:20 +0530

    Update broadcast.py

--- BrandrdXMusic/plugins/misc/broadcast.py
+++ BrandrdXMusic/plugins/misc/broadcast.py
@@ -25,7 +25,7 @@ IS_BROADCASTING = False
 async def braodcast_message(client, message, _):
     if message.from_user.id != OWNER_ID:
         return await message.reply_text(
-            "» **sɪʀғ ʏᴇʜ @BRANDRD_BOT ʙʀᴏᴀᴅᴄᴀsᴛ ᴋᴀʀ sᴀᴋᴛᴀ ʜᴀɪ 😏**\n» ᴊᴏɪɴ @BRANDED_WORLD ғᴏʀ ᴘʀᴏᴍᴏ"
+            "» ** ඔන්න @illluminatimusic_bot ʙʀᴏᴀᴅᴄᴀsᴛ කරා හොදේ 😏**\n» ᴊᴏɪɴ @illumnati1 එකට ADD වෙන්න"
         )
     global IS_BROADCASTING
     if message.reply_to_message:
